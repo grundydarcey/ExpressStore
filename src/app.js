a6ccc8917d4ee2b9a44ec744a63a3743bb83cd85
Beyonce Knowles <you@example.com>
2021-01-04 12:36:49 -0500

    first commit

--- src/app.js
+++ src/app.js
@@ -16,8 +16,9 @@ app.use(morgan(morganOption));
 app.use(helmet());
 app.use(cors());
 
-app.get('/', (req, res) => {
-  res.send('Hello, world!');
+app.post('/', (req, res) => {
+  res
+    .send('POST request received.');
 });
 
 app.use(function errorHandler(error, req, res, next) {
